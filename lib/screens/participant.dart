5b6eb5a5021fc1f18e1d3c7578361d48da3b409a
Vani Shivanand <vani.shivanand@gmail.com>
2019-12-22 16:16:04 +0530

    adding the first draft

---
+++ lib/screens/participant.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class ParticipantWidget extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    
+    return ParticipantState();
+  }
+  
+}
+
+class ParticipantState extends State<ParticipantWidget>{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Text("hello");
+  }
+  
+}
